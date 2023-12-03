5ee4fc2ea8b953289080bacbec4271b9753e0d05
Carl <qwerfcxzas1@gmail.com>
2023-12-03 10:49:35 +0800

    Moidyf

---
+++ done/next13-discord-clone/next13-discord-clone.sql
@@ -0,0 +1,35 @@
+-- --------------------------------------------------------
+-- 主機:                           127.0.0.1
+-- 伺服器版本:                        10.4.28-MariaDB - mariadb.org binary distribution
+-- 伺服器作業系統:                      Win64
+-- HeidiSQL 版本:                  12.6.0.6765
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- 取消選取資料匯出。
+
+-- 取消選取資料匯出。
+
+-- 取消選取資料匯出。
+
+-- 取消選取資料匯出。
+
+-- 取消選取資料匯出。
+
+-- 取消選取資料匯出。
+
+-- 取消選取資料匯出。
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- text.md
+++ text.md
@@ -1,8 +1,9 @@
-# stripe webhook setting 
+# stripe webhook setting
 
 ```bash
 stripe login
 ```
+
 ```bash
 stripe listen --forward-to localhost:3000/api/webhook
 stripe listen --forward-to localhost:3000/api/webhooks/stripe
@@ -10,6 +11,7 @@ stripe listen --forward-to localhost:3000/api/webhooks/stripe
 
 stripe_tes_card= 4242424242424242
 333
+
 ```
 stripe trigger payment_intent.succeeded
 ```
